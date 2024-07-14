05a30736557ebb9f1125ced22189ccdb9b211dce
tunoa.johnson <tunoajohnson@gmail.com>
2024-07-14 12:08:53 +1000

    Disable for now

--- ESP32/include/WifiProvisionManager.h
+++ ESP32/include/WifiProvisionManager.h
@@ -1,27 +1,27 @@
-#pragma once
+// #pragma once
 
-#include <WiFi.h>
-#include <WiFiProv.h>
+// #include <WiFi.h>
+// #include <WiFiProv.h>
 
-class WiFiProvisionManager {
-public:
-    WiFiProvisionManager();
-    void begin();
-    void update();
-    bool isProvisioned() const { return _isProvisioned; }
-    bool isConnected() const { return _isConnected; }
-    void startProvisioning();
-    bool isProvisioning() const { return _isProvisioning; }
+// class WiFiProvisionManager {
+// public:
+//     WiFiProvisionManager();
+//     void begin();
+//     void update();
+//     bool isProvisioned() const { return _isProvisioned; }
+//     bool isConnected() const { return _isConnected; }
+//     void startProvisioning();
+//     bool isProvisioning() const { return _isProvisioning; }
 
-private:
-    static void wifiEventCallback(arduino_event_t *event);
-    static void provisionEventCallback(arduino_event_t *event, WiFiProvisionManager *instance);
-    void handleProvisionEvent(arduino_event_t *event);
-    void connectToWiFi();
+// private:
+//     static void wifiEventCallback(arduino_event_t *event);
+//     static void provisionEventCallback(arduino_event_t *event, WiFiProvisionManager *instance);
+//     void handleProvisionEvent(arduino_event_t *event);
+//     void connectToWiFi();
 
-    bool _isProvisioned;
-    bool _isConnected;
-    bool _isProvisioning;
+//     bool _isProvisioned;
+//     bool _isConnected;
+//     bool _isProvisioning;
 
-    static WiFiProvisionManager* instance;
-};
+//     static WiFiProvisionManager* instance;
+// };
--- ESP32/src/WiFiProvisionManager.cpp
+++ ESP32/src/WiFiProvisionManager.cpp
@@ -1,79 +1,79 @@
-#include "WiFiProvisionManager.h"
-#include <esp_wifi.h>
+// #include "WiFiProvisionManager.h"
+// #include <esp_wifi.h>
 
-static const char PROGMEM service_name[] = "TempControl_PROV";
-static const char PROGMEM pop[] = "abcd1234";
+// static const char PROGMEM service_name[] = "TempControl_PROV";
+// static const char PROGMEM pop[] = "abcd1234";
 
-WiFiProvisionManager* WiFiProvisionManager::instance = nullptr;
+// WiFiProvisionManager* WiFiProvisionManager::instance = nullptr;
 
-WiFiProvisionManager::WiFiProvisionManager() 
-    : _isProvisioned(false), _isConnected(false), _isProvisioning(false) {
-    instance = this;
-}
+// WiFiProvisionManager::WiFiProvisionManager() 
+//     : _isProvisioned(false), _isConnected(false), _isProvisioning(false) {
+//     instance = this;
+// }
 
-void WiFiProvisionManager::begin() {
-    WiFi.onEvent(wifiEventCallback);
+// void WiFiProvisionManager::begin() {
+//     WiFi.onEvent(wifiEventCallback);
 
-    if (WiFi.isConnected()) {
-        _isProvisioned = true;
-        _isConnected = true;
-    }
-}
+//     if (WiFi.isConnected()) {
+//         _isProvisioned = true;
+//         _isConnected = true;
+//     }
+// }
 
-void WiFiProvisionManager::update() {
-    if (_isProvisioned && !_isConnected) {
-        connectToWiFi();
-    }
-    _isConnected = WiFi.isConnected();
-}
+// void WiFiProvisionManager::update() {
+//     if (_isProvisioned && !_isConnected) {
+//         connectToWiFi();
+//     }
+//     _isConnected = WiFi.isConnected();
+// }
 
-void WiFiProvisionManager::startProvisioning() {
-    if (!_isProvisioning) {
-        WiFiProv.beginProvision(WIFI_PROV_SCHEME_BLE, WIFI_PROV_SCHEME_HANDLER_FREE_BTDM, 
-                                WIFI_PROV_SECURITY_1, pop, service_name);
+// void WiFiProvisionManager::startProvisioning() {
+//     if (!_isProvisioning) {
+//         WiFiProv.beginProvision(WIFI_PROV_SCHEME_BLE, WIFI_PROV_SCHEME_HANDLER_FREE_BTDM, 
+//                                 WIFI_PROV_SECURITY_1, pop, service_name);
         
-        WiFi.onEvent([](arduino_event_t *event) {
-            provisionEventCallback(event, instance);
-        }, ARDUINO_EVENT_PROV_START);
+//         WiFi.onEvent([](arduino_event_t *event) {
+//             provisionEventCallback(event, instance);
+//         }, ARDUINO_EVENT_PROV_START);
 
-        _isProvisioning = true;
-    }
-}
+//         _isProvisioning = true;
+//     }
+// }
 
-void WiFiProvisionManager::wifiEventCallback(arduino_event_t *event) {
-    if (instance) {
-        switch (event->event_id) {
-            case ARDUINO_EVENT_WIFI_STA_GOT_IP:
-                instance->_isConnected = true;
-                break;
-            case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
-                instance->_isConnected = false;
-                break;
-            default:
-                break;
-        }
-    }
-}
+// void WiFiProvisionManager::wifiEventCallback(arduino_event_t *event) {
+//     if (instance) {
+//         switch (event->event_id) {
+//             case ARDUINO_EVENT_WIFI_STA_GOT_IP:
+//                 instance->_isConnected = true;
+//                 break;
+//             case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
+//                 instance->_isConnected = false;
+//                 break;
+//             default:
+//                 break;
+//         }
+//     }
+// }
 
-void WiFiProvisionManager::provisionEventCallback(arduino_event_t *event, WiFiProvisionManager *instance) {
-    if (instance) {
-        instance->handleProvisionEvent(event);
-    }
-}
+// void WiFiProvisionManager::provisionEventCallback(arduino_event_t *event, WiFiProvisionManager *instance) {
+//     if (instance) {
+//         instance->handleProvisionEvent(event);
+//     }
+// }
 
-void WiFiProvisionManager::handleProvisionEvent(arduino_event_t *event) {
-    switch (event->event_id) {
-        case ARDUINO_EVENT_PROV_CRED_RECV:
-            _isProvisioned = true;
-            break;
-        case ARDUINO_EVENT_PROV_END:
-            _isProvisioning = false;
-            break;
-        default:
-            break;
-    }
-}
+// void WiFiProvisionManager::handleProvisionEvent(arduino_event_t *event) {
+//     switch (event->event_id) {
+//         case ARDUINO_EVENT_PROV_CRED_RECV:
+//             _isProvisioned = true;
+//             break;
+//         case ARDUINO_EVENT_PROV_END:
+//             _isProvisioning = false;
+//             break;
+//         default:
+//             break;
+//     }
+// }
 
-void WiFiProvisionManager::connectToWiFi() {
-    WiFi.begin();
-}
+// void WiFiProvisionManager::connectToWiFi() {
+//     WiFi.begin();
+// }
